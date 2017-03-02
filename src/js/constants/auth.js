b83257c2ec5d21a88a41ef03629df837fdae50ce
bruce mcpherson <bruce@mcpher.com>
2017-03-02 08:58:38 +0000

    change to default service

--- src/js/constants/auth.js
+++ src/js/constants/auth.js
@@ -16,7 +16,7 @@ export default {
 	apiBase: {
 	    dev:'https://nodestuff-xlibersion.c9users.io',
 	    alpha:'http://v1.effexapi.org',
-	    prod:'https://api-dot-ephex-auth.appspot-preview.com' 
+	    prod:'https://ephex-auth.appspot-preview.com' 
 	},
 	
 	effexAdmin:"fd99a5d6e073e8c0aa2f7bd2877b5578df7c70f5b48632a37a7bf2aeee950364",
